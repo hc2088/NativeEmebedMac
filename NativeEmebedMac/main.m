48fa048cd3318f7fe28dc49ad8a65c46e67a05cb
huchu <huchu@58.com>
2021-11-05 20:10:29 +0800

    123

---
+++ NativeEmebedMac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NativeEmebedMac
+//
+//  Created by huchu on 2021/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
